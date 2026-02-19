fcb2e80d0bb10f9c0097687ffe0ada7dd17e1f40
Brian Bosire Evans <evanbosire422@gmail.com>
2026-02-19 12:49:08 +0300

    string functions validation

--- 07stringFunctions.py
+++ 07stringFunctions.py
@@ -4,7 +4,7 @@ print(type(name))
 
 # Types
 age = 24
-print(type(age)) # -> type()
+print(type(age))  # -> type()
 print("Your age is:" + str(age))  # -> str()
 
 # Math
@@ -30,7 +30,8 @@ print(amount.replace("$", "").replace(",", ""))
 # Convert "+49 (176) 123 - 4567" to 00491761234567
 
 number = "+49 (176) 123 - 4567"
-print(number.replace("+", "00").replace(" ", "").replace("-", "").replace("(", "").replace(")", ""))
+print(number.replace("+", "00").replace(" ",
+      "").replace("-", "").replace("(", "").replace(")", ""))
 
 # String Concantenation
 
@@ -50,7 +51,8 @@ name = "Brian"
 age = 34
 is_student = False
 
-print(f"My name is {name}, I am {age} years old, and student status is {is_student}")
+print(
+    f"My name is {name}, I am {age} years old, and student status is {is_student}")
 
 print(f"2 + 3 = {2 + 3}")
 
@@ -82,16 +84,84 @@ print(date[-2:])
 
 # Cleaning String Values
 
+# lstrip() -> removes white spaces from the left
+# rstrip() -> removes white spaces from the right
+# strip() -> removes white spaces from both sides.
+
+text = " Engineering"
+text = "Engineering "
+text = " Engineering "
+text = "###Abc##"
+
+print(text.lstrip())
+print(text.rstrip())
+print(text.strip())
+print(text.strip("#"))  # -> When passing args to the strip method,
+#    it removes those characters from a string
 
 
+#   Case Conversions
+text = "python PROGRAMMING"
+
+print(text.lower())
+print(text.upper())
+print(text.capitalize())    # First letter capital
+
+# challenge
+# "968-Maria, ( D@t@ Engineer ) ;; 27y  " -> name: maria | role: data engineer | age: 27
+
+# solution
+text = "968-Maria, ( D@t@ Engineer ) ;; 27y  "
+print(text.strip().replace("@", "a"))
+name = text.strip()[4:9].lower()
+role = text.strip()[13:26].lower().replace("@", "a")
+age = text.strip()[-3:-1].lower()
+
+print(f"name: {name} | role: {role} | age: {age}")
+
+print("#" * 60)
 
 
+text1 = "968-Maria, ( D@t@ Engineer ) ;; 27y"
+print(f"{text1}")
+clean = text1.replace("@", "a").strip()
+print(clean)
+# Split parts
+id_name, rest = clean.split(",", 1)
+
+print(id_name, rest)
+
+role_part, age_part = rest.split(";;")
+print(role_part, age_part)
 
 
+# String Searching
 
 
+date = "2026-Feb-10"
+url = "https://api.company/v1/data"
+phone1 = "+48-176-12345"
+phone2 = "48-654-16548"
+phone3 = "0048-654-16548"
 
+print(date.startswith("2026"))
+print(date.endswith("10"))
+print("Feb" in date)
+print(f"Find 10 is at index: {date.find("10")}")
+print("/api" in url)
 
+print(phone1[4:])
+print(phone2[3:])
+# solution instead of counting the indexes
 
+print(phone1[phone1.find("-")+1:])
+print(phone2[phone2.find("-")+1:])
+print(phone3[phone3.find("-")+1:])
 
+# String functions validation
 
+country = "KENYA"
+phone = "012367654@"
+
+print(country.isalpha())    # checks is the string has only alphabets a-z A-Z
+print(phone.isnumeric())    # checks if phone is a number
