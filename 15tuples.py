4a3907c7732a15b2df06611002722b7075384790
Brian Bosire Evans <evanbosire422@gmail.com>
2026-02-28 20:25:36 +0300

    dealt with tuples

---
+++ 15tuples.py
@@ -0,0 +1,24 @@
+# Tuple -> An ordered collection that can't be changed after creating it
+
+my_tuple = (10, 30, 20, 10)
+
+print(my_tuple) # ordered # allows duplicates
+print(my_tuple[1]) # indexed
+
+#my_tuple[3] = 40 # immutable
+
+
+# USE CASE: Connecting to DB
+
+
+
+
+
+
+
+
+
+
+
+
+
